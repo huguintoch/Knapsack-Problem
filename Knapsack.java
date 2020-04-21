2a4ec8d1a72c12cf33cd1eccc42cbc6dc31474b1
emilio2399 <49567212+emilio2399@users.noreply.github.com>
2020-04-20 20:53:43 -0500

    Testeo

--- Knapsack.java
+++ Knapsack.java
@@ -1,89 +1,99 @@
-
 // Java program to solve fractional Knapsack Problem 
 import java.util.Arrays; 
 import java.util.Comparator; 
-
+  
 // Greedy approach 
-public class Knapsack
+public class Knapsack 
 { 
-	// Time complexity O(n log n) 
-	public static void main(String[] args) 
-	{ 
-		int[] wt = {10, 40, 50, 30}; 
-		int[] val = {100, 40, 250, 120}; 
-		int capacity = 50; 
+	
+	static long startTime = System.nanoTime();
 
-		double maxValue = getMaxValue(wt, val, capacity); 
-		System.out.println("Maximum value we can obtain = " + 
-							maxValue); 
-
-	} 
-
-	// function to get maximum value 
-	private static double getMaxValue(int[] wt, 
-						int[] val, int capacity) 
-	{ 
-		ItemValue[] iVal = new ItemValue[wt.length]; 
-
-		for(int i = 0; i < wt.length; i++) 
-		{ 
-			iVal[i] = new ItemValue(wt[i], val[i], i); 
-		} 
-
-		//sorting items by value; 
-		Arrays.sort(iVal, new Comparator<ItemValue>() 
-		{ 
-			@Override
-			public int compare(ItemValue o1, ItemValue o2) 
-			{ 
-				return o2.cost.compareTo(o1.cost) ; 
-			} 
-		}); 
+	// code
 
 
-		double totalValue = 0d; 
 
-		for(ItemValue i: iVal) 
-		{ 
-
-			int curWt = (int) i.wt; 
-			int curVal = (int) i.val; 
-
-			if (capacity - curWt >= 0) 
-			{ 
-				// this weight can be picked while 
-				capacity = capacity-curWt; 
-				totalValue += curVal; 
-
-			} 
-			else
-			{ 
-				// item cant be picked whole 
-				double fraction = ((double)capacity/(double)curWt); 
-				totalValue += (curVal*fraction); 
-				capacity = (int)(capacity - (curWt*fraction)); 
-				break; 
-			} 
-
-
-		} 
-
-		return totalValue; 
-	} 
-
-	// item value class 
-	static class ItemValue 
-	{ 
-		Double cost; 
-		double wt, val, ind; 
-		
-		// item value function 
-		public ItemValue(int wt, int val, int ind) 
-		{ 
-			this.wt = wt; 
-			this.val = val; 
-			this.ind = ind; 
-			cost = new Double(val/wt ); 
-		} 
-	} 
+  
+    // function to get maximum value 
+    private static double getMaxValue(int[] wt, 
+                        int[] val, int capacity) 
+    { 
+        ItemValue[] iVal = new ItemValue[wt.length]; 
+  
+        for(int i = 0; i < wt.length; i++) 
+        { 
+            iVal[i] = new ItemValue(wt[i], val[i], i); 
+        } 
+  
+        //sorting items by value; 
+        Arrays.sort(iVal, new Comparator<ItemValue>()  
+        { 
+            @Override
+            public int compare(ItemValue o1, ItemValue o2)  
+            { 
+                return o2.cost.compareTo(o1.cost) ; 
+            } 
+        }); 
+  
+  
+        double totalValue = 0d; 
+  
+        for(ItemValue i: iVal) 
+        { 
+  
+            int curWt = (int) i.wt; 
+            int curVal = (int) i.val; 
+  
+            if (capacity - curWt >= 0) 
+            { 
+                // this weight can be picked while 
+                capacity = capacity-curWt; 
+                totalValue += curVal; 
+  
+            } 
+            else
+            { 
+                // item cant be picked whole 
+                double fraction = ((double)capacity/(double)curWt); 
+                totalValue += (curVal*fraction); 
+                capacity = (int)(capacity - (curWt*fraction)); 
+                break; 
+            } 
+  
+  
+        } 
+  
+        return totalValue; 
+    } 
+  
+    // item value class 
+    static class ItemValue  
+    { 
+        Double cost; 
+        double wt, val, ind; 
+          
+        // item value function 
+        public ItemValue(int wt, int val, int ind) 
+        { 
+            this.wt = wt; 
+            this.val = val; 
+            this.ind = ind; 
+            cost = new Double(val/wt ); 
+        } 
+    } 
+    
+    // Time complexity O(n log n) 
+    public static void main(String[] args) 
+    { 
+        int[] wt = {10, 40, 20, 30}; 
+        int[] val = {60, 40, 100, 120}; 
+        int capacity = 50; 
+  
+        double maxValue = getMaxValue(wt, val, capacity); 
+        System.out.println("Maximum value we can obtain = " +  
+                            maxValue); 
+        
+    	long endTime = System.nanoTime();
+    	System.out.println("Took "+(endTime - startTime) + " ns"); 
+  
+    } 
 } 
