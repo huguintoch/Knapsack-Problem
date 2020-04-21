bdb881056fbc3cb735f5b34a98cea603891acaa3
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2020-04-20 23:03:27 -0500

    Archivo cambiado

--- KnapsackDynamicProgramming.java
+++ KnapsackDynamicProgramming.java
@@ -1,110 +1,143 @@
 // Reference: https://www.educative.io/courses/grokking-dynamic-programming-patterns-for-coding-interviews/RM1BDv71V60#top-down-dynamic-programming-with-memoization
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
 class KnapsackDynamicProgramming {
 
-  static long startTime = System.nanoTime();
+	static long startTime = System.nanoTime();
+	
+	private static int[][] dpMain;
 
-  public int solveKnapsack(int[] profits, int[] weights, int capacity) {
-    // basic checks
-    if (capacity <= 0 || profits.length == 0 || weights.length != profits.length)
-      return 0;
+	public int solveKnapsack(int[] profits, int[] weights, int capacity) {
+		// basic checks
+		if (capacity <= 0 || profits.length == 0 || weights.length != profits.length)
+			return 0;
 
-    int n = profits.length;
-    int[][] dp = new int[n][capacity + 1];
+		int n = profits.length;
+		int[][] dp = new int[n][capacity + 1];
 
-    // populate the capacity=0 columns, with '0' capacity we have '0' profit
-    for(int i=0; i < n; i++)
-      dp[i][0] = 0;
+		// populate the capacity=0 columns, with '0' capacity we have '0' profit
+		for(int i=0; i < n; i++)
+			dp[i][0] = 0;
 
-    // if we have only one weight, we will take it if it is not more than the capacity
-    for(int c=0; c <= capacity; c++) {
-      if(weights[0] <= c)
-        dp[0][c] = profits[0];
-    }
+		// if we have only one weight, we will take it if it is not more than the capacity
+		for(int c=0; c <= capacity; c++) {
+			if(weights[0] <= c)
+				dp[0][c] = profits[0];
+		}
 
-    // process all sub-arrays for all the capacities
-    for(int i=1; i < n; i++) {
-      for(int c=1; c <= capacity; c++) {
-        int profit1= 0, profit2 = 0;
-        // include the item, if it is not more than the capacity
-        if(weights[i] <= c)
-          profit1 = profits[i] + dp[i-1][c-weights[i]];
-        // exclude the item
-        profit2 = dp[i-1][c];
-        // take maximum
-        dp[i][c] = Math.max(profit1, profit2);
-      }
-    }
+		// process all sub-arrays for all the capacities
+		for(int i=1; i < n; i++) {
+			for(int c=1; c <= capacity; c++) {
+				int profit1= 0, profit2 = 0;
+				// include the item, if it is not more than the capacity
+				if(weights[i] <= c)
+					profit1 = profits[i] + dp[i-1][c-weights[i]];
+				// exclude the item
+				profit2 = dp[i-1][c];
+				// take maximum
+				dp[i][c] = Math.max(profit1, profit2);
+			}
+		}
+		
+		
+		dpMain = dp;
+		return dp[n-1][capacity];
+	}
 
-    this.printSelectedElements(dp, weights, profits, capacity);
+	private static int[] printSelectedElements(int dp[][], int[] weights, int[] profits, int capacity){
+		int[] used = new int[weights.length];
+		System.out.print("Selected weights:");
+		int totalProfit = dp[weights.length-1][capacity];
+		int index = 0;
+		for(int i=weights.length-1; i > 0; i--) {
+			if(totalProfit != dp[i-1][capacity]) {
+				used[index] = weights[i];
+				capacity -= weights[i];
+				totalProfit -= profits[i];
+				index++;
+			}
+		}
+		if(totalProfit != 0)
+			System.out.print(" " + weights[0]);
+		System.out.println("");
+		return used;
+	}
 
-    // maximum profit will be at the bottom-right corner.
-    return dp[n-1][capacity];
-  }
+	public static void main(String[] args) {
 
-  private void printSelectedElements(int dp[][], int[] weights, int[] profits, int capacity){
-    System.out.print("Selected weights:");
-    int totalProfit = dp[weights.length-1][capacity];
-    for(int i=weights.length-1; i > 0; i--) {
-      if(totalProfit != dp[i-1][capacity]) {
-        System.out.print(" " + weights[i]);
-        capacity -= weights[i];
-        totalProfit -= profits[i];
-      }
-    }
-    if(totalProfit != 0)
-      System.out.print(" " + weights[0]);
-    System.out.println("");
-  }
+		int maxProfit = 0;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(args[0]));
 
-  public static void main(String[] args) {
+			String[] s = br.readLine().split(" "); // Get n
+			int n = Integer.parseInt(s[0]);
 
-    try {
-		  BufferedReader br = new BufferedReader(new FileReader(args[0]));
+			System.out.println(n);
 
-      String[] s = br.readLine().split(" "); // Get n
-  		int n = Integer.parseInt(s[0]);
+			s = br.readLine().split(" "); // Get profits
+			int[] profits = new int[n];
+			for(int i=0; i<n; i++){
+				profits[i] = Integer.parseInt(s[i]);
+			}
 
-      System.out.println(n);
+			System.out.println(Arrays.toString(profits));
 
-      s = br.readLine().split(" "); // Get profits
-      int[] profits = new int[n];
-      for(int i=0; i<n; i++){
-        profits[i] = Integer.parseInt(s[i]);
-      }
+			s = br.readLine().split(" "); // Get weights
+			int[] weights = new int[n];
+			for(int i=0; i<n; i++){
+				weights[i] = Integer.parseInt(s[i]);
+			}
 
-      System.out.println(Arrays.toString(profits));
+			System.out.println(Arrays.toString(weights));
 
-      s = br.readLine().split(" "); // Get weights
-      int[] weights = new int[n];
-      for(int i=0; i<n; i++){
-        weights[i] = Integer.parseInt(s[i]);
-      }
+			s = br.readLine().split(" "); // Get maxWeight
+			int w = Integer.parseInt(s[0]);
 
-      System.out.println(Arrays.toString(weights));
+			System.out.println(w);
 
-      s = br.readLine().split(" "); // Get maxWeight
-      int w = Integer.parseInt(s[0]);
-      
-      System.out.println(w);
+			KnapsackDynamicProgramming ks = new KnapsackDynamicProgramming();
+			maxProfit = ks.solveKnapsack(profits, weights, w);
 
-      KnapsackDynamicProgramming ks = new KnapsackDynamicProgramming();
-      int maxProfit = ks.solveKnapsack(profits, weights, w);
+			System.out.println("Total knapsack profit:" + maxProfit);      
 
-      System.out.println("Total knapsack profit:" + maxProfit);      
+			br.close();
+			try {
+				BufferedWriter writer = new BufferedWriter(new FileWriter("resultados.txt"));
+				writer.write("MaxProfit: " + maxProfit + "\n");
+				int[] used = printSelectedElements(dpMain, weights, profits, w);
+				writer.write("Weights used: ");
+				for(int i=0;i<used.length;i++) {
+					if(used[i]!=0) {
+						writer.write(used[i] + " ");
+					}else {
+						writer.write("\n");
+						break;
+					}
+				}
+				writer.write("Matrix: \n");
+				for(int i=0;i<dpMain.length;i++) {
+					for(int j=0;j<dpMain.length;j++) {
+						writer.write(dpMain[i][j] + " ");
+					}
+					writer.write("\n");
+				}
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-		  br.close();
 		}catch(FileNotFoundException e){
 			System.out.println("No se localizó el archivo " + e);
 		}catch(IOException e){
 			System.out.println("Ocurrió un error de I/O" + e);
 		}
-
-  }
+	}
 }
