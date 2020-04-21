5c2d66018aaa524f247989ae00a22ebe38738ba5
Memohto <42613170+Memohto@users.noreply.github.com>
2020-04-21 07:22:28 -0500

    For para revertir los arreglos eliminado

--- Knapsack.java
+++ Knapsack.java
@@ -3,10 +3,10 @@ public class Knapsack {
     static double startTime = System.nanoTime();
 
     public static void main(String[] args) {
-        Integer[] weights = {2,3,5,6,4};
-        Integer[] values = {6,1,12,8,7};
-        int n = 5;
-        int totalWeight = 11;
+        Integer[] weights = {10, 40, 20, 30};
+        Integer[] values = {60, 40, 100, 120};
+        int n = 4;
+        int totalWeight = 50;
         Greedy(totalWeight, values, weights, n);
         double endTime = System.nanoTime();
         System.out.println("Took "+(endTime - startTime)/1000000000 + " sec");
@@ -19,23 +19,11 @@ public class Knapsack {
         }
 
         Sort.quicksort(densities, values, weights);
-        
-        for(int i=0; i<densities.length/2; i++){
-            Integer tempValue = values[i];
-            values[i] = values[values.length-i-1];
-            values[values.length-i-1] = tempValue;
-            Integer tempWeight = weights[i];
-            weights[i] = weights[weights.length-i-1];
-            weights[weights.length-i-1] = tempWeight;
-            Float tempDensity = densities[i];
-            densities[i] = densities[densities.length-i-1];
-            densities[densities.length-i-1] = tempDensity;
-        }
 
         boolean[] solution = new boolean[n];
 
         int sumWeight = maxWeight;
-        for(int i = 0; i < n && weights[i] <= sumWeight; i++) {
+        for(int i = n-1; i >= 0 && weights[i] <= sumWeight; i--) {
             solution[i] = true;
             sumWeight = sumWeight - weights[i];
         }
