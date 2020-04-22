a3f53d286d68264003dc4cff81539148f649bac3
Memohto <42613170+Memohto@users.noreply.github.com>
2020-04-21 21:08:18 -0500

    Input recibido y resultados escritos en txt

--- Knapsack.java
+++ Knapsack.java
@@ -1,74 +1,82 @@
 import java.io.*;
 
-public class Knapsack{
+public class Knapsack {
 
     static double startTime = System.nanoTime();
 
-    public static void main(final String[] args) {
-
-
-       /* Integer[] weights = {10, 40, 20, 30};
-        Integer[] values = {60, 40, 100, 120};
-        int n = 4;
-        int totalWeight = 50; */
-
+    public static void main(String[] args) {
         try {
-            final BufferedReader br = new BufferedReader(new FileReader(args[0]));
-    
-        String[] s = br.readLine().split(" "); // Get n
-            final int n = Integer.parseInt(s[0]);
-    
-        s = br.readLine().split(" "); // Get profits
-        final int[] profits = new int[n];
-        for(int i=0; i<n; i++){
-          profits[i] = Integer.parseInt(s[0]);
-        }
-    
-        s = br.readLine().split(" "); // Get weights
-        final int[] weights = new int[n];
-        for(int i=0; i<n; i++){
-          weights[i] = Integer.parseInt(s[0]);
-        }
-    
-        s = br.readLine().split(" "); // Get maxWeight
-            final int w = Integer.parseInt(s[0]);
-    
-            Greedy(w, profits, weights, n);
-            final double endTime = System.nanoTime();
-            System.out.println("Took "+(endTime - startTime)/1000000000 + " sec");
-    
-            br.close();
-          }catch(final FileNotFoundException e){
-              System.out.println("No se localizó el archivo " + e);
-          }catch(final IOException e){
-              System.out.println("Ocurrió un error de I/O" + e);
-          }
+            //BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            BufferedReader br = new BufferedReader(new FileReader("Prueba.txt"));
 
-       
+            String[] s = br.readLine().split(" "); // Get n
+            int n = Integer.parseInt(s[0]);
+
+            s = br.readLine().split(" "); // Get profits
+            Integer[] profits = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                profits[i] = Integer.parseInt(s[i]);
+            }
+
+            s = br.readLine().split(" "); // Get weights
+            Integer[] weights = new Integer[n];
+            for (int i = 0; i < n; i++) {
+                weights[i] = Integer.parseInt(s[i]);
+            }
+
+            s = br.readLine().split(" "); // Get maxWeight
+            int w = Integer.parseInt(s[0]);
+
+            Greedy(w, profits, weights, n);
+            double endTime = System.nanoTime();
+            System.out.println("Took " + (endTime - startTime) / 1000000000 + " sec");
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se localizó el archivo " + e);
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error de I/O" + e);
+        }
     }
 
-    public static void Greedy(final int maxWeight, final Integer[] values, final Integer[] weights, final Integer n) {
-        final Float[] densities = new Float[n];
+    public static void Greedy(int maxWeight, Integer[] profits, Integer[] weights, int n) {
+        Float[] densities = new Float[n];
         for(int i = 0; i < n; i++) {
-            densities[i] = (float)values[i]/weights[i];
+            densities[i] = (float)profits[i]/weights[i];
         }
 
-        Sort.quicksort(densities, values, weights);
+        Sort.quicksort(densities, profits, weights);
+        
+        boolean[] solution = new boolean[n];
+        int sumWeight = maxWeight;
 
-        final for(int i = n-1; i >= 0 && weights[i] <= sumWeight; i--) {
+        for(int i = n-1; i >= 0 && weights[i] <= sumWeight; i--) {
             solution[i] = true;
             sumWeight = sumWeight - weights[i];
         }
 
-        int finalBenefit = 0;
+        int maxProfit = 0;
+        int[] used = new int[n];
         for(int i = 0; i < solution.length; i++) {
             if(solution[i]) {
-                finalBenefit += values[i];
-                System.out.println("Object with value: "+values[i]+" , weight: "+weights[i]+" and density: "+densities[i]);
+                maxProfit += profits[i];
+                used[i] = weights[i];
             }
         }
 
-        System.out.println("Final benefit: "+finalBenefit);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("resultados.txt"));
+            writer.write("MaxProfit: " + maxProfit + "\n");
+            writer.write("Weights used: ");
+            for(int i=0;i<used.length;i++) {
+                if(used[i] != 0)
+                    writer.write(used[i] + " ");
+            }
+            writer.write("\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error de I/O" + e);
+        }
     }
 
 }
