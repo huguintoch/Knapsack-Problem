ea5bad7a1228a48ce7795a96a480cd66f3a07bb8
emilio2399 <49567212+emilio2399@users.noreply.github.com>
2020-04-22 23:00:42 -0500

    shellSort

--- Sort.java
+++ Sort.java
@@ -34,4 +34,58 @@ public class Sort {
 		valores[i] = valores[j];
 		valores[j] = temp;
 	}
+	
+/*public class shellSort { 
+	// An utility function to print array of size 
+	static void printArray(float[] arr) { 
+		int n = arr.length; 
+		for (int i = 0; i < n; ++i) 
+			System.out.print(arr[i] + " "); 
+			System.out.println(); 
+	} 
+	  
+	// function to sort arr using shellSort 
+	private static float sort(float arr[]) { 
+		int n = arr.length; 
+	  
+		// Start with a big gap, then reduce the gap 
+		for (int gap = n / 2; gap > 0; gap /= 2) { 
+			// Do a gapped insertion sort for this gap size. 
+			// The first gap elements a[0..gap-1] are already 
+			// in gapped order keep adding one more element 
+			// until the entire array is gap sorted 
+			for (int i = gap; i < n; i += 1) { 
+				// add a[i] to the elements that have been gap 
+				// sorted save a[i] in temp and make a hole at 
+				// position i 
+				float temp =   arr[i]; 
+	  
+				// shift earlier gap-sorted elements up until 
+				// the correct location for a[i] is found 
+				int j; 
+				for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
+					arr[j] = arr[j - gap]; 
+	  
+				// put temp (the original a[i]) in its correct 
+				// location 
+				arr[j] = temp; 
+			} 
+		} 
+		return 0; 
+	} 
+	  
+		// Driver method 
+	public static void main(String args[]) { 
+		float arr[] = { 12, 34, 54, 2, 3,1.0f, 1.54f, 254.4f, 128.4f, 1.001f, 1.02f}; 
+		System.out.println("Array before sorting"); 
+		printArray(arr); 
+		System.out.println("");
+	  
+	
+		sort(arr); 
+	  
+		System.out.println("Array after sorting"); 
+		printArray(arr); 
+	} 
+} */
 }
