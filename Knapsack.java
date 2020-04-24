1e8bfa587f50dba5cb0a9e6947f3402d01e2aab1
Memohto <42613170+Memohto@users.noreply.github.com>
2020-04-23 19:28:34 -0500

    Cambios que pidió el profe

--- Knapsack.java
+++ Knapsack.java
@@ -13,13 +13,13 @@ public class Knapsack {
             int n = Integer.parseInt(s[0]);
 
             s = br.readLine().split(" "); // Get profits
-            Integer[] profits = new Integer[n];
+            int[] profits = new int[n];
             for (int i = 0; i < n; i++) {
                 profits[i] = Integer.parseInt(s[i]);
             }
 
             s = br.readLine().split(" "); // Get weights
-            Integer[] weights = new Integer[n];
+            int[] weights = new int[n];
             for (int i = 0; i < n; i++) {
                 weights[i] = Integer.parseInt(s[i]);
             }
@@ -39,38 +39,32 @@ public class Knapsack {
         }
     }
 
-    public static void Greedy(int maxWeight, Integer[] profits, Integer[] weights, int n) {
+    public static void Greedy(int maxWeight, int[] profits, int[] weights, int n) {
         Float[] densities = new Float[n];
+        Integer[] positions = new Integer[n];
         for(int i = 0; i < n; i++) {
             densities[i] = (float)profits[i]/weights[i];
+            positions[i] = i;
         }
 
-        Sort.quicksort(densities, profits, weights);
+        Sort.mergesort(densities, positions);
         
         boolean[] solution = new boolean[n];
         int sumWeight = maxWeight;
-
-        for(int i = n-1; i >= 0 && weights[i] <= sumWeight; i--) {
-            solution[i] = true;
-            sumWeight = sumWeight - weights[i];
-        }
-
         int maxProfit = 0;
-        int[] used = new int[n];
-        for(int i = 0; i < solution.length; i++) {
-            if(solution[i]) {
-                maxProfit += profits[i];
-                used[i] = weights[i];
-            }
+        for(int i = n-1; i >= 0 && weights[positions[i]] <= sumWeight; i--) {
+            solution[i] = true;
+            maxProfit += profits[positions[i]];
+            sumWeight = sumWeight - weights[positions[i]];
         }
 
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("resultados.txt"));
-            writer.write("MaxProfit: " + maxProfit + "\n");
-            writer.write("Weights used: ");
-            for(int i=0;i<used.length;i++) {
-                if(used[i] != 0)
-                    writer.write(used[i] + ", ");
+            writer.write(maxProfit + "\n");
+            for(int i=0;i<solution.length;i++) {
+                if(solution[i]) {
+                    writer.write(positions[i].toString()+" ");
+                } 
             }
             writer.write("\n");
             writer.close();
@@ -78,5 +72,4 @@ public class Knapsack {
             System.out.println("Ocurrió un error de I/O" + e);
         }
     }
-
 }
--- Sort.java
+++ Sort.java
@@ -1,31 +1,30 @@
 
 public class Sort {
-	public static <E extends Comparable<E>> void quicksort(E[] datos, Integer[] datos1, Integer[] datos2) {
-		quicksort(datos, datos1, datos2, 0, datos.length);
+	//Quick Sort
+	public static <E extends Comparable<E>> void quicksort(E[] datos, Integer[] datos1) {
+		quicksort(datos, datos1, 0, datos.length);
 	}
 	
-	private static <E extends Comparable<E>> void quicksort(E[] datos, Integer[] datos1, Integer[] datos2, int left,int right) {
+	private static <E extends Comparable<E>> void quicksort(E[] datos, Integer[] datos1, int left,int right) {
 		if(left < right) {
-			int p = particionar(datos, datos1, datos2, left, right);
-			quicksort(datos, datos1, datos2, left, p);
-			quicksort(datos, datos1, datos2, p+1, right);
+			int p = particionar(datos, datos1, left, right);
+			quicksort(datos, datos1, left, p);
+			quicksort(datos, datos1, p+1, right);
 		}
 	}
 	
-	private static <E extends Comparable<E>> int particionar(E[] datos, Integer[] datos1, Integer[] datos2, int l, int r) {
+	private static <E extends Comparable<E>> int particionar(E[] datos, Integer[] datos1, int l, int r) {
 		E p = datos[l];
 		int i = l + 1;
 		for(int j = l + 1; j < r; j++) {
 			if(datos[j].compareTo(p) <= 0) {
                 swap(datos, i, j);
                 swap(datos1, i, j);
-                swap(datos2, i, j);
 				i++;
 			}
 		}
         swap(datos, l, i-1);
         swap(datos1, l, i-1);
-        swap(datos2, l, i-1);
 		return i-1;
 	}
 	
@@ -34,58 +33,67 @@ public class Sort {
 		valores[i] = valores[j];
 		valores[j] = temp;
 	}
+
+	//Merge Sort
+	public static <E extends Comparable<E>> void mergesort(E[] datos, Integer[]datos1) {
+		mergesort(datos, datos1, 0, datos.length-1);
+	}
 	
-/*public class shellSort { 
-	// An utility function to print array of size 
-	static void printArray(float[] arr) { 
-		int n = arr.length; 
-		for (int i = 0; i < n; ++i) 
-			System.out.print(arr[i] + " "); 
-			System.out.println(); 
-	} 
-	  
-	// function to sort arr using shellSort 
-	private static float sort(float arr[]) { 
-		int n = arr.length; 
-	  
-		// Start with a big gap, then reduce the gap 
-		for (int gap = n / 2; gap > 0; gap /= 2) { 
-			// Do a gapped insertion sort for this gap size. 
-			// The first gap elements a[0..gap-1] are already 
-			// in gapped order keep adding one more element 
-			// until the entire array is gap sorted 
-			for (int i = gap; i < n; i += 1) { 
-				// add a[i] to the elements that have been gap 
-				// sorted save a[i] in temp and make a hole at 
-				// position i 
-				float temp =   arr[i]; 
-	  
-				// shift earlier gap-sorted elements up until 
-				// the correct location for a[i] is found 
-				int j; 
-				for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) 
-					arr[j] = arr[j - gap]; 
-	  
-				// put temp (the original a[i]) in its correct 
-				// location 
-				arr[j] = temp; 
-			} 
-		} 
-		return 0; 
-	} 
-	  
-		// Driver method 
-	public static void main(String args[]) { 
-		float arr[] = { 12, 34, 54, 2, 3,1.0f, 1.54f, 254.4f, 128.4f, 1.001f, 1.02f}; 
-		System.out.println("Array before sorting"); 
-		printArray(arr); 
-		System.out.println("");
-	  
+	private static <E extends Comparable<E>> void mergesort(E[] datos, Integer[]datos1, int ini, int fin) {
+		int mid;
+		if(ini < fin) {
+			mid = (ini+fin)/2;
+			mergesort(datos, datos1, ini, mid);
+			mergesort(datos, datos1, mid+1, fin);
+			merge(datos, datos1, ini, fin);
+		}
+	}
 	
-		sort(arr); 
-	  
-		System.out.println("Array after sorting"); 
-		printArray(arr); 
-	} 
-} */
+	private static <E extends Comparable<E>> void merge(E[] datos, Integer[]datos1, int ini, int fin) {
+		int mid = (ini+fin)/2;
+		E[] tempLeft = (E[]) new Comparable[mid-ini+1] ;
+		E[] tempRight = (E[]) new Comparable[fin-mid] ;
+		Integer[] tempLeft1 = new Integer[mid-ini+1] ;
+		Integer[] tempRight1 = new Integer[fin-mid] ;
+		
+		for(int i = 0; i < mid-ini+1; i++) {
+			tempLeft[i] = datos[ini+i];
+			tempLeft1[i] = datos1[ini+i];
+		}
+		for(int i = 0; i < fin-mid; i++) {
+			tempRight[i] = datos[mid+1+i];
+			tempRight1[i] = datos1[mid+1+i];
+		}
+		
+		int cont = ini,
+			i = 0,
+			j = 0;
+		
+		while(i < mid-ini+1 && j < fin-mid ) {
+			if(tempLeft[i].compareTo(tempRight[j]) <= 0) {
+				datos[cont] = tempLeft[i];
+				datos1[cont] = tempLeft1[i];
+				i++;
+			} else {
+				datos[cont] = tempRight[j];
+				datos1[cont] = tempRight1[j];
+				j++;
+			}
+			cont++;
+		}
+		
+		while(i < mid-ini+1) {
+			datos[cont] = tempLeft[i];
+			datos1[cont] = tempLeft1[i];
+			i++;
+			cont++;
+		}
+		
+		while(j < fin-mid) {
+			datos[cont] = tempRight[j];
+			datos1[cont] = tempRight1[j];
+			j++;
+			cont++;
+		}
+	}
 }
