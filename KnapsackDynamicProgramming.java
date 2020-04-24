e11f7490f143ef674cda14cfd7353b101e4942bb
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2020-04-23 19:58:14 -0500

    Validacion lista

--- KnapsackDynamicProgramming.java
+++ KnapsackDynamicProgramming.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
 
 class KnapsackDynamicProgramming {
 
@@ -16,15 +15,10 @@ class KnapsackDynamicProgramming {
 
 	public static int solveKnapsack(int[] profits, int[] weights, int capacity) {
 		// Edge cases
-		if (capacity <= 0 || profits.length == 0 || weights.length != profits.length)
-			return 0;
 
 		int n = profits.length;
 		int[][] dp = new int[n][capacity + 1];
 
-		// Fill profits with 0 when weight is 0
-		for(int i=0; i < n; i++)
-			dp[i][0] = 0;
 		// If we have only one weight, we will take it if it is not more than the capacity
 		for(int c=0; c <= capacity; c++) {
 			if(weights[0] <= c)
@@ -74,35 +68,109 @@ class KnapsackDynamicProgramming {
 			BufferedReader br = new BufferedReader(new FileReader(args[0]));
 
 			String[] s = br.readLine().split(" "); // Get n
-			int n = Integer.parseInt(s[0]);
+			int n=0;
+			try {
+				n = Integer.parseInt(s[0]);
+			}catch (NumberFormatException e) {
+				System.out.println("La cantidad de elementos en la mochila debe ser un número.");
+				br.close();
+				return;
+			}
+			
 			//System.out.println("# of elements: " + n);
 
 			s = br.readLine().split(" "); // Get profits
 			int[] profits = new int[n];
-			for(int i=0; i<n; i++){
-				profits[i] = Integer.parseInt(s[i]);
+			try {
+				for(int i=0; i<n; i++){
+					profits[i] = Integer.parseInt(s[i]);
+				}
+			}catch(NumberFormatException e) {
+				System.out.println("El beneficio de todos los objetos debe ser representado con un número.");
+				br.close();
+				return;
 			}
+			
 			//System.out.println("Profits: " + Arrays.toString(profits));
 
 			s = br.readLine().split(" "); // Get weights
 			int[] weights = new int[n];
-			for(int i=0; i<n; i++){
-				weights[i] = Integer.parseInt(s[i]);
+			try {
+				for(int i=0; i<n; i++){
+					weights[i] = Integer.parseInt(s[i]);
+					if(weights[i]==0) {
+						System.out.println("El peso del objeto en el indice " + i + " no puede ser 0.");
+						br.close();
+						return;
+					}
+				}
+			}catch(NumberFormatException e) {
+				System.out.println("El peso de todos los objetos debe ser mayor a 0.");
+				br.close();
+				return;
 			}
+			
+			
+			
 			//System.out.println("Weights: " + Arrays.toString(weights));
 
 			s = br.readLine().split(" "); // Get maxWeight
-			int w = Integer.parseInt(s[0]);
-			//System.out.println("Maximum weight: " + w);
-
-			maxProfit = KnapsackDynamicProgramming.solveKnapsack(profits, weights, w);
+			int w = 0;
+			try {
+				w = Integer.parseInt(s[0]);
+				if(w==0) {
+					System.out.println("El peso máximo de la mochila debe ser mayor a 0.");
+					br.close();
+					return;
+				}
+			}catch (NumberFormatException e){
+				System.out.println("El peso máximo de la mochila debe ser representado con un número.");
+				br.close();
+				return;
+			}
+			
+			
+			if(weights.length != profits.length) {
+				System.out.println("Se debe introducir una misma cantidad de pesos como de beneficios.");
+				br.close();
+				return;
+			}
+			
+			if(profits.length==0 || weights.length==0) {
+				System.out.println("Se debe ingresar por lo menos un peso y un beneficio.");
+				br.close();
+				return;
+			}
+			
+			
+			try {
+				maxProfit = KnapsackDynamicProgramming.solveKnapsack(profits, weights, w);
+			}catch(NumberFormatException e) {
+				System.out.println("Los números generados no caben dentro de un entero.");
+				br.close();
+				return;
+			}
+			
 			//System.out.println("Total knapsack profit: " + maxProfit);      
 
       		br.close();
       
 			try {
-				BufferedWriter writer = new BufferedWriter(new FileWriter("resultados.txt"));
-				writer.write("MaxProfit: " + maxProfit + "\n");
+				int file = 0;
+				file = args[0].charAt(3);
+
+				if(file==0) 
+					file = 10;
+				
+				FileWriter fileWriter;
+				if(file!=10) {
+					fileWriter = new FileWriter("ga0"+file+".txt");
+				}else {
+					fileWriter = new FileWriter("ga"+file+".txt");
+				}
+				
+				BufferedWriter writer = new BufferedWriter(fileWriter);
+				writer.write(maxProfit + "\n");
 				int[] used = getSelectedElements(dpMain, weights, profits, w);
 				writer.write("Weights used: ");
 				for(int i=0;i<used.length;i++) {
